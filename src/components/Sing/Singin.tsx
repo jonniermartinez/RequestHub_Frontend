1c38ba27d0c736708ca11acd267995943aba2bad
Jonnier <jonnierosjitas@gmail.com>
2023-08-22 22:46:08 -0500

    auth page strutures

--- src/components/Sing/Singin.tsx
+++ src/components/Sing/Singin.tsx
@@ -1,10 +1,9 @@
 import { Input } from "../ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "../ui/button";
-import "./Login.css";
 import { client } from "@/supabase";
 
-function Login() {
+export default function SingIn() {
   const handleSingIn = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     try {
@@ -21,15 +20,15 @@ function Login() {
   };
   return (
     <>
-      <div className="grid w-full max-w-sm items-center gap-1.5">
+      <div className="grid w-full max-w-sm max-h-96 items-center gap-1.5">
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-          Login
+          SingIn
         </h2>
         <Label htmlFor="email">Email</Label>
         <Input type="email" id="email" placeholder="Email" />
         <Label htmlFor="password">Password</Label>
         <Input type="password" id="password" placeholder="Password"></Input>
-        <Button onClick={handleSingIn}>Login</Button>
+        <Button onClick={handleSingIn}>SingIn</Button>
         <Button variant="outline">
           {/* hacer un componenete icono */}
           <img
@@ -37,10 +36,9 @@ function Login() {
             src="https://cdn-icons-png.flaticon.com/512/2702/2702602.png"
             alt=""
           />
-          Login with Email
+          SingIn with Email
         </Button>
       </div>
     </>
   );
 }
-export default Login;
--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,10 +1,37 @@
 import { Login } from "@/components";
+import SingIn from "@/components/Sing/Singin";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-function Auth() {
+export default function Auth() {
   return (
-    <div>
-      <Login></Login>
-    </div>
+    <>
+      <div className="flex gap-11">
+        <div className=" w-5/12">
+          {/* Sara aqui va tu compoente image */}
+          <img
+            src="https://res.cloudinary.com/dtd4ibgoz/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1692712582/image_25_dxlig4.jpg?_s=public-apps"
+            alt=""
+            className=" max-h-screen w-full object-scale-contain"
+          />
+        </div>
+        <div className="flex w-1/2 justify-center">
+          <Tabs
+            defaultValue="account"
+            className="w-[400px] h-screen flex flex-col justify-center "
+          >
+            <TabsList className=" w-fit">
+              <TabsTrigger value="account">Login</TabsTrigger>
+              <TabsTrigger value="password">SingIn</TabsTrigger>
+            </TabsList>
+            <TabsContent value="account" className=" h-1">
+              <Login></Login>
+            </TabsContent>
+            <TabsContent value="password">
+              <SingIn></SingIn>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </>
   );
 }
-export default Auth;
