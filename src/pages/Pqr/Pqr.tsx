a7b616657144fa05c511a5905c93a2c00205e006
Jonnier <jonnierosjitas@gmail.com>
2023-09-20 19:16:39 -0500

    fix: imageCompo erros

--- src/pages/Pqr/Pqr.tsx
+++ src/pages/Pqr/Pqr.tsx
@@ -1,13 +1,8 @@
-import PQRCompo from "@/components/pqrCompo/pqrCompo";
 import { useParams } from "react-router-dom";
 
 function Pqr() {
   const { id } = useParams();
   console.log(id);
-  return (
-    <div>
-      <PQRCompo empresa="Jonnier SAS"></PQRCompo>
-    </div>
-  );
+  return <div></div>;
 }
 export default Pqr;
