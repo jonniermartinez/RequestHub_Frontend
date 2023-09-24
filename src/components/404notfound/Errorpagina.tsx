19ff3b1665d77691e88a0322d37ed50cbfadb12d
Jonnier Martinez <jonnierosjitas@gmail.com>
2023-09-24 18:09:23 -0500

    fix: 🐛 rename the component and fix the titles style scope

--- src/components/404notfound/Errorpagina.tsx
+++ src/components/404notfound/Errorpagina.tsx
@@ -1,19 +1,17 @@
-import React from "react"
-
 import "./estilo.css";
 
 
 function Errorpagina() {
   return (
     <div className="contenedor-principal">
-      <h1> ¡oops!</h1> <br />
+      <h1 className="title"> ¡oops!</h1> <br />
       <br />
       <p className="texto-principal">404 Not Found </p> <br />
 
       <div className="contenedor-final">
         <p className="texto-final"> ¡¡ no se encuentra lo que estas buscando !!</p> <br />
-       <a href="#" classname="btn">Go To Home</a>
-      </div>  
+        <a href="#" className="btn">Go To Home</a>
+      </div>
 
     </div>
   )
