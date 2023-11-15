8f00bdbb81f06d8d180adad8ebb60acf2699569a
Jonnier Martinez <jonnierosjitas@gmail.com>
2023-11-14 23:27:46 -0500

    ya va tomando cara

--- src/components/DashboardComponent/DashboardComponent.tsx
+++ src/components/DashboardComponent/DashboardComponent.tsx
@@ -1,46 +1,136 @@
-import './DashboardComponent.css'
-import { LogOut } from '..';
-import { client } from "@/supabase";
+// import { LogOut } from '..';
+import { client } from '@/supabase';
 import { useEffect, useState } from 'react';
+import { Button } from '..';
+import logo from '../../../public/icon.svg';
+import {
+  SignOut,
+  User,
+  Cube,
+  ChartLine,
+  Toolbox,
+  MagnifyingGlass,
+  Bell,
+} from '@phosphor-icons/react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { TableDemo } from '../TableDemo/TableDemo';
 
 export default function DashboardComponent() {
-    // const userState = useSelector((store: RootState) => store.user);
-    //     console.log(userState.data);
-    const [ id, setId ] = useState(String); 
-    useEffect(() => {
-        client.auth
-          .getSession()
-          .then((data) => {
-            if (data.data.session != null) {
-                setId(data.data.session.user.id);
-            }
-        })
-        .catch((error) => console.log(error));
-    }, []);
-    
-    console.log(id);
-    return (
-        <div className="dashboard">
-            <div className="sidebar">
-                <h2>Menú</h2>
-                <ul>
-                    <li>Inicio</li>
-                    <li>Notificaciones</li>
-                    <li>Configuración esto </li>
-                    <LogOut></LogOut>
-                </ul>
-            </div>
-            <div className="content">
-                <h1>Bienvenido al Dashboard</h1>
-                <div className="widget">
-                    <h2>Widget 1</h2>
-                    <p>Contenido del Widget 1</p>
-                </div>
-                <div className="widget">
-                    <h2>Widget 2</h2>
-                    <p>Contenido del Widget 2</p>
-                </div>
-            </div>
+  const [id, setId] = useState(String);
+  useEffect(() => {
+    client.auth
+      .getSession()
+      .then((data) => {
+        if (data.data.session != null) {
+          setId(data.data.session.user.id);
+        }
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log(id);
+  return (
+    <div className="flex bg-stone-900 h-screen">
+      <div className="flex flex-col items-center justify-between w-[7%] h-screen py-5 ">
+        <img src={logo} alt="" className=" w-9 p-1 bg-white rounded-lg" />
+        <div className="flex flex-col gap-10">
+          <Toolbox size={25} color="white"></Toolbox>
+          <ChartLine size={25} color="white"></ChartLine>
+          <Cube size={25} color="white"></Cube>
+          <User size={25} color="white"></User>
         </div>
-    );
+        <SignOut size={25} color="white"></SignOut>
+        {/* <LogOut></LogOut> */}
+      </div>
+      {/* numbers section */}
+      <div className="bg-white m-3 w-[93%] rounded-2xl p-9">
+        <nav className="flex justify-between items-center mb-6">
+          <p className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
+            Overview
+          </p>
+          <div className="flex items-center gap-5">
+            <MagnifyingGlass size={20}></MagnifyingGlass>
+            <Bell size={20}></Bell>
+            <Avatar className=" w-8 h-8">
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+        </nav>
+        <section className="flex flex-col gap-12">
+          <div className="h-52 flex gap-16">
+            <div className="w-[35%] bg-white h-full">
+              <p className="scroll-m-20 text-xl font-semibold tracking-tights">
+                Porfolio
+              </p>
+              <div className="flex h-full">
+                <Card
+                  textPrincipal="$17.000"
+                  texSecundary="total pqrs"
+                  className="bg-blue-100"
+                ></Card>
+              </div>
+            </div>
+            <div className="w-[65%] bg-white h-full">
+              <p className="scroll-m-20 text-xl font-semibold tracking-tights">
+                Your assets
+              </p>
+              <div className="flex h-full gap-10">
+                <Card
+                  textPrincipal="1.25 BTC"
+                  texSecundary="$898980"
+                  className="bg-violet-200"
+                ></Card>
+                <Card
+                  textPrincipal="1.25 BTC"
+                  texSecundary="$898980"
+                  className="bg-green-200"
+                ></Card>
+                <Card
+                  textPrincipal="1.25 BTC"
+                  texSecundary="$898980"
+                  className="bg-yellow-200"
+                ></Card>
+              </div>
+            </div>
+          </div>
+          <div className="flex h-10">
+            <div className="w-[60%]">
+              <TableDemo></TableDemo>
+            </div>
+            <div className="w-[30%] p-10 flex flex-col gap-4 h-fit ml-20 m-10 rounded-xl bg-slate-950">
+              <p className="scroll-m-20 text-xl font-semibold tracking-tights text-white">
+                Earn Free Crytop With CoinView Earn!
+              </p>
+              <span className="text-xs inline-block text-white ">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </span>
+              <Button>sadas</Button>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
+
+interface CardProps {
+  textPrincipal: string;
+  texSecundary: string;
+  className: string;
+}
+
+const Card = ({
+  textPrincipal,
+  texSecundary,
+  className,
+}: CardProps): JSX.Element => {
+  return (
+    <div className={'w-full h-full mt-5 rounded-xl p-5 ' + className}>
+      <p className="scroll-m-20 text-xl font-semibold tracking-tights">
+        {textPrincipal}
+      </p>
+      <span className="text-xs">{texSecundary}</span>
+    </div>
+  );
+};
--- src/pages/Pqr/Pqr.tsx
+++ src/pages/Pqr/Pqr.tsx
@@ -1,38 +1,36 @@
-import { useParams } from "react-router-dom";
-import { PQRCompo } from "@/components";
-import { client } from "@/supabase";
-import { useState, useEffect } from "react";
-import Errorpagina from "@/components/404notfound/Errorpagina";
+import { useParams } from 'react-router-dom';
+import { PQRCompo } from '@/components';
+import { client } from '@/supabase';
+import { useState, useEffect } from 'react';
+import Errorpagina from '@/components/404notfound/Errorpagina';
 
 //http://localhost:5173/pqr/2d11eba4-24c5-46b2-b34b-3d78f171a850/sara
 function Pqr() {
-  const [existProfile, setExistProfile] = useState(Boolean)
+  const [existProfile, setExistProfile] = useState(Boolean);
   // const { id, name } = useParams();
   const { id } = useParams();
   // verificar si el id pertenece a alguna organizacion de nuestra base datos
   useEffect(() => {
-    verifyTheId()
-  }, [id])
+    verifyTheId();
+  }, [id]);
 
   async function verifyTheId() {
     try {
-      const { data, error } = await client
-        .from("profiles")
-        .select("id")
-        .eq("id", id)
-
-      data ? setExistProfile(true) : setExistProfile(false)
-
+      const { data } = await client.from('profiles').select('id').eq('id', id);
+      data ? setExistProfile(true) : setExistProfile(false);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-
   }
 
-  return <div>
-    {
-      existProfile ? <PQRCompo empresa="Jonnier" profileId={id}></PQRCompo> : <Errorpagina />
-    }
-  </div>;
+  return (
+    <div>
+      {existProfile ? (
+        <PQRCompo empresa="Jonnier" profileId={id}></PQRCompo>
+      ) : (
+        <Errorpagina />
+      )}
+    </div>
+  );
 }
 export default Pqr;
