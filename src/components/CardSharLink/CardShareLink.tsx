c0b70341836579df09675b29e825925d597cadd7
01GlennAM <glennalverez@gmail.com>
2023-11-22 20:41:08 -0500

    cambios

--- src/components/CardSharLink/CardShareLink.tsx
+++ src/components/CardSharLink/CardShareLink.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
+import './cardShareLink.css';
+import { Button } from '@/components/ui/button';
 
 function CardShareLink() {
-  return <div>CardShareLink</div>;
+  return <div>
+    <div className=' card'>
+      <h1>  Share this link with your clients !  </h1> 
+        <p> ¡This is the link to the PQR form. Access the form here and share your feedback and requests with us!</p>
+       
+        <CopyToClipboard text="glenn"
+          >
+        <Button>Copy Url</Button>
+        </CopyToClipboard>
+         
+        
+        
+    </div>
+    
+  </div>;
 }
 
 export default CardShareLink;
