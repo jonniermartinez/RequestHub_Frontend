40dcf0565afdf8af00003b7b4937f8cde8f3e3fd
Jonnier Martinez <jonnierosjitas@gmail.com>
2023-11-23 22:42:22 -0500

    sara: cambios top

--- src/components/CardSharLink/CardShareLink.tsx
+++ src/components/CardSharLink/CardShareLink.tsx
@@ -1,24 +1,43 @@
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import './cardShareLink.css';
 import { Button } from '@/components/ui/button';
+import { getUserId } from '@/utilities/getUserId';
+import { useEffect, useState } from 'react';
 
 function CardShareLink() {
-  return <div>
-    <div className=' card'>
-      <h1>  Share this link with your clients !  </h1> 
-        <p> ¡This is the link to the PQR form. Access the form here and share your feedback and requests with us!</p>
-       
-        <CopyToClipboard text="glenn"
-          >
-        <Button>Copy Url</Button>
+  const [url, setUrl] = useState(String);
+
+  useEffect(() => {
+    //
+
+    const getId = async () => {
+      const data = await getUserId();
+      if (window.location.host == 'localhost:5173') {
+        setUrl('http://localhost:5173/pqr/' + data + '/hq');
+      } else {
+        setUrl('https://requesthubhq.netlify.app/pqr/' + data + '/hq');
+      }
+    };
+    getId();
+  }, [url]);
+
+  return (
+    <div>
+      <div className=" card">
+        <h1> Share this link with your clients ! </h1>
+        <p>
+          {' '}
+          ¡This is the link to the PQR form. Access the form here and share your
+          feedback and requests with us!
+        </p>
+
+        <CopyToClipboard text={url}>
+          <Button>Copy Url</Button>
         </CopyToClipboard>
-         
-        
-        
+      </div>
     </div>
-    
-  </div>;
+  );
 }
 
 export default CardShareLink;
--- src/utilities/getUserId.ts
+++ src/utilities/getUserId.ts
@@ -7,5 +7,6 @@ export async function getUserId() {
     return '2d11eba4-24c5-46b2-b34b-3d78f171a850';
   } catch (error) {
     console.log(error);
+    return '';
   }
 }
